ac3f26f8dae41e2f06b2c5b1c21ce6e699b7649e
thezayin <zainshahidbuttt@gmail.com>
2024-08-20 10:28:50 +0500

    Refactor code

--- core/common/src/main/java/com/thezayin/common/dailogs/ErrorQueryDialog.kt
+++ core/common/src/main/java/com/thezayin/common/dailogs/ErrorQueryDialog.kt
@@ -1,13 +1,11 @@
-package com.thezayin.paksimdata.presentation.activities.dialogs
+package com.thezayin.common.dailogs
 
-import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
@@ -16,23 +14,20 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.colorResource
-import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.compose.ui.window.Dialog
-import com.thezayin.paksimdata.R
 
 @Composable
-fun NetworkDialog(showDialog: (Boolean) -> Unit) {
-    Dialog(onDismissRequest = {}) {
+fun ErrorDialog(showDialog: (Boolean) -> Unit = {}, callback: () -> Unit = {}, error: String) {
+    Dialog(onDismissRequest = { }) {
         Surface(
             shape = RoundedCornerShape(16.dp),
-            color = Color.White
+            color = colorResource(id = com.thezayin.values.R.color.white),
         ) {
             Box(
                 contentAlignment = Alignment.Center
@@ -41,39 +36,27 @@ fun NetworkDialog(showDialog: (Boolean) -> Unit) {
 
                     Spacer(modifier = Modifier.height(20.dp))
                     Text(
-                        text = "No internet connection",
-                        fontFamily = FontFamily(Font(com.thezayin.core.R.font.abeezee_regular)),
-                        color = colorResource(id = com.thezayin.core.R.color.black),
+                        text = error,
+                        fontFamily = FontFamily(Font(com.thezayin.font.R.font.abeezee_regular)),
+                        color = colorResource(id = com.thezayin.values.R.color.text_color),
                         fontSize = 16.sp,
                         modifier = Modifier.fillMaxWidth(),
                         textAlign = TextAlign.Center
                     )
                     Spacer(modifier = Modifier.height(20.dp))
-                    Box(
-                        modifier = Modifier
-                            .padding(horizontal = 40.dp)
-                            .fillMaxWidth(),
-                        contentAlignment = Alignment.Center
-                    ) {
-                        Image(
-                            painter = painterResource(id = com.thezayin.core.R.drawable.ic_connection),
-                            contentDescription = null,
-                            modifier = Modifier.size(50.dp)
-                        )
-                    }
-                    Spacer(modifier = Modifier.height(20.dp))
                     Button(
                         onClick = {
                             showDialog(false)
+                            callback()
                         },
                         modifier = Modifier.fillMaxWidth(),
                         shape = RoundedCornerShape(6.dp),
-                        colors = ButtonDefaults.buttonColors(contentColor = colorResource(id = com.thezayin.core.R.color.black))
+                        colors = ButtonDefaults.buttonColors(contentColor = colorResource(id = com.thezayin.values.R.color.black))
                     ) {
                         Text(
-                            text = "Retry",
-                            fontFamily = FontFamily(Font(com.thezayin.core.R.font.abeezee_italic)),
-                            color = colorResource(id = com.thezayin.core.R.color.white),
+                            text = "Done",
+                            fontFamily = FontFamily(Font(com.thezayin.font.R.font.abeezee_regular)),
+                            color = colorResource(id = com.thezayin.values.R.color.white),
                             fontSize = 16.sp,
                             textAlign = TextAlign.Center
                         )
