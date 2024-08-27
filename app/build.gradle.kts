1a548d71f92d6ea9a13561147292d7913feff668
thezayin <zainshahidbuttt@gmail.com>
2024-08-27 09:19:18 +0500

    Bump version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -19,8 +19,8 @@ android {
         applicationId = "com.thezayin.paksimdata"
         minSdk = 24
         targetSdk = 35
-        versionCode = 6
-        versionName = "1.0.6"
+        versionCode = 7
+        versionName = "1.0.7"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
